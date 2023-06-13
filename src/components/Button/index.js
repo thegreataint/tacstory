af41f682145d7cd08ce99b4d54e7a8fae4e5afda
MohamedAbu El-Nour <thegreataint@metalfan.com>
2023-06-13 22:01:29 +0300

    fix(button): Change button to js instead of jsx

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -19,6 +19,9 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   );
 };
 
+
+
+
 Button.propTypes = {
   /**
    * Is this the principal call to action on the page?
